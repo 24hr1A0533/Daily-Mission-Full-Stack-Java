00b922df94f4a1f76402ef7254dc1d98e192081c
24hr1A0533 <kingsofmcg@gmail.com>
2026-07-02 07:20:45 +0530

    mm

--- Day3/Ternary.java
+++ Day3/Ternary.java
@@ -2,6 +2,7 @@ public class Ternary {
     public static void main(String[]args){
         int a =20;
         int b=20;
+    
         String result= a==b ? "both are equal" : "both are not equal";
         System.out.println(result);
     }
